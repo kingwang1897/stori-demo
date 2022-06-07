01fcf05d7401856f8c96489121cd7b123ace4770
kingwang1897 <king.wang@storicard.com>
2022-06-07 14:40:11 +0800

    add close registry

--- web/src/main/java/com/stori/sofa/controller/TesCloseRegistryController.java
+++ web/src/main/java/com/stori/sofa/controller/TesCloseRegistryController.java
@@ -1,16 +1,15 @@
 package com.stori.sofa.controller;
 
-import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alipay.sofa.rpc.context.RpcRuntimeContext;
+import com.google.common.base.Throwables;
 
 /**
- * close registry controller
+ * Close Registry Controller
  * 
  * @author king
  * @date 2022/06/07 19:24
@@ -21,17 +20,20 @@ public class TesCloseRegistryController {
     private static final Logger LOGGER = LoggerFactory.getLogger(TesCloseRegistryController.class);
 
     /**
-     * close test function
+     * Close Registry
      *
      * @date 2022/05/05 19:25
      * @return java.lang.String
      */
-    @GetMapping("/test/close")
-    public String testClose() throws IOException {
-        long time1 = System.currentTimeMillis();
-        RpcRuntimeContext.destroy();
-        LOGGER.info("testClose success, time is:{}, costTime is: {}.", System.currentTimeMillis(),
-            System.currentTimeMillis() - time1);
-        return null;
+    @GetMapping("/close/registry")
+    public void closeRegistry() {
+        try {
+            long time1 = System.currentTimeMillis();
+            RpcRuntimeContext.destroy();
+            LOGGER.info("Close Registry success, time is:{}, costTime is: {}.", System.currentTimeMillis(),
+                System.currentTimeMillis() - time1);
+        } catch (Exception e) {
+            LOGGER.error("Close Registry error, case by:{}.", Throwables.getStackTraceAsString(e));
+        }
     }
 }
